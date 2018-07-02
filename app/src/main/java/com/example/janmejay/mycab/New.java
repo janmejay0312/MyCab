8d0bfce5f4820864d5b0ddb753d9e7b647750733
janmejay0312 <janmejay351@gmail.com>
2018-07-02 21:36:17 +0530

    me

--- app/src/main/java/com/example/janmejay/mycab/New.java
+++ app/src/main/java/com/example/janmejay/mycab/New.java
@@ -2,16 +2,24 @@ package com.example.janmejay.mycab;
 
 import android.arch.persistence.room.*;
 import android.arch.persistence.room.Room;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 
 public class New extends AppCompatActivity {
 public  static android.support.v4.app.FragmentManager fragmentManager;
 public static MyDatabase myDatabase;
+private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         fragmentManager=getSupportFragmentManager();
         myDatabase= Room.databaseBuilder(getApplication(),MyDatabase.class,"userDb").allowMainThreadQueries().build();
         if(findViewById(R.id.fragment)!=null){
@@ -21,5 +29,13 @@ public static MyDatabase myDatabase;
             }
             fragmentManager.beginTransaction().add(R.id.fragment,new Home()).commit();
         }
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.app_bar_menu,menu);
+        return true;
+    }
+
 }
